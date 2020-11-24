d5da1db47363eaf8a78c9a505a8ed5aed003bd53
Gurpreet-18 <58430618+Gurpreet-18@users.noreply.github.com>
2020-11-24 08:59:08 +0100

    Add files via upload

---
+++ SentimentAnalysis/myapp/secrets.py
@@ -0,0 +1,9 @@
+#this file contains sensitive information like API secret key
+
+
+class Oauth_Secrets:
+    def __init__(self):
+        self.consumer_key = "UxCmyVhasGqhAsBHO0KffSC4b"
+        self.consumer_secret ="Lr9lOYM4GvG5ypEojEEBE3IuQUyCRSMsgRVEEqmXlkGEvEoQk8"
+        self.access_token ="1318214672814931969-TDXvKXEnHkLfoCuOBjRFQbla68k0kk"
+        self.access_token_secret ="y8g3k017ZNzh4R5BhqMxlISuxU7P8b0yRkYtLAKwXvDes"
