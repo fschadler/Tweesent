5b793a7f0fc2878056656a9f18a0c5660a5eacf1
Gurpreet-18 <58430618+Gurpreet-18@users.noreply.github.com>
2020-12-01 13:37:53 +0100

    Update urls.py

--- TwitterSentimentAnalyser/TSentimentAnalyser/App_TwitterDataAnalyser/urls.py
+++ TwitterSentimentAnalyser/TSentimentAnalyser/App_TwitterDataAnalyser/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Courses, name ="home-page"),
+    path('', views.home, name ="home-page"),
+    path('contact.html', views.contact, name="contact")
 ]
