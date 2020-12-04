cdef42a2a6794e033a0ecd9c8a9afa40083b620b
Gurpreet-18 <58430618+Gurpreet-18@users.noreply.github.com>
2020-12-04 19:53:13 +0100

    Update urls.py

--- TwitterSentimentAnalyser/TSentimentAnalyser/App_TwitterDataAnalyser/urls.py
+++ TwitterSentimentAnalyser/TSentimentAnalyser/App_TwitterDataAnalyser/urls.py
@@ -3,6 +3,5 @@ from . import views
 from App_TwitterTrends.views import trends_form_view
 
 urlpatterns = [
-    path('', views.home, name ="home-page"),
-    path('contact.html', views.contact, name="contact")
+   path('', views.Contact, name="Contact"),
 ]
