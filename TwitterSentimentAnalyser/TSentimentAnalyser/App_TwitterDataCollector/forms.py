f70f0e431ef4498c32f065a61b14644f47180feb
SerJaeger <72598618+SerJaeger@users.noreply.github.com>
2020-12-10 19:34:12 +0100

    Overhaul of Design and simplifying template code Kept old css files for now. Content stayed the same, Output Page is not reworked yet.

--- TwitterSentimentAnalyser/TSentimentAnalyser/App_TwitterDataCollector/forms.py
+++ TwitterSentimentAnalyser/TSentimentAnalyser/App_TwitterDataCollector/forms.py
@@ -8,11 +8,11 @@ class TwitterForm(forms.Form):
      User Input on homepage which is used to filter tweet-search.
      """
      hashtag = forms.CharField(label='Hashtag or Word', max_length=30, widget=forms.TextInput
-                                (attrs={'placeholder': '#Hashtag or SearchTerm'}))
-     num_terms = forms.CharField(label='Number of Tweets', widget=forms.NumberInput(attrs={'placeholder': '100', "min": 1, "max": 500}))
+                                (attrs={'placeholder': '#Sentiment', "class": "form-control"}))
+     num_terms = forms.CharField(label='Number of Tweets', widget=forms.NumberInput(attrs={'placeholder': '100',"min": 1, "max": 500, "class": "form-control"}))
 
 class LocationForm(forms.Form):
     """
     User Input on homepage which is used to show location-based trends.
     """
-    location = forms.CharField(label='Your location', max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Enter City/Country'}))
+    location = forms.CharField(label='', max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Enter City/Country', "class": "form-control"}))
